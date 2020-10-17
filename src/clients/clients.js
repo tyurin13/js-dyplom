f421d48d59fe920975802000f10bd8036d9c3cf3
tyurin13 <batistabomb2010@gmail.com>
2020-10-17 22:14:36 +0300

    fetch

--- src/clients/clients.js
+++ src/clients/clients.js
@@ -22,6 +22,11 @@ let staff = [{name: 'Alex', company: 'Google', email: 'alex@gmail.com', phone: '
 
 export function createClientsTable() {
   ROOT_NODE.innerHTML = template;
-  clientsDataBase(staff);
-  
+  fetch("https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((staff) => {
+      clientsDataBase(staff);
+    })
 }
